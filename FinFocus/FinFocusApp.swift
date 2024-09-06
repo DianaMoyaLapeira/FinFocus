2aa92d029f2d5cdfc6692bf0cee7fbcaa168a5fb
Diana Moya Lapeira <dianamoyalapeira@gmail.com>
2024-09-06 18:50:32 -0400

    Initial Commit

---
+++ FinFocus/FinFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinFocusApp.swift
+//  FinFocus
+//
+//  Created by Diana Moya Lapeira on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FinFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
