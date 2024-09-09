1a1c83b08f0ebc1a56435ea51316dfedc8934669
Diana Moya Lapeira <dianamoyalapeira@gmail.com>
2024-09-08 21:14:47 -0400

    add font and stub MVVM

---
+++ LogInView.swift
@@ -0,0 +1,20 @@
+//
+//  LogInView.swift
+//  FinFocus
+//
+//  Created by Diana Moya Lapeira on 9/8/24.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .font(.custom("DM_Sans", size: 32))
+            .fontWeight(.black)
+    }
+}
+
+#Preview {
+    LogInView()
+}
